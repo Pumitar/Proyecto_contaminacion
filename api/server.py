db0ebfee691d2365603367eafe7f01b6fae26629
Cesar Puma <puma.rasec@yahoo.es>
2025-08-21 11:41:56 -0500

    deploy

--- api/server.py
+++ api/server.py
@@ -1,15 +1,23 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
+import os
 
 
 from api.controlers import get_ranking
 
-app = Flask('Proyecto_Cosmo', template_folder='public/templates', static_folder='public/static')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES = os.path.join(BASE_DIR, 'public', 'templates')
+STATIC = os.path.join(BASE_DIR, 'public', 'static')
+
+app = Flask(__name__, template_folder=TEMPLATES, static_folder=STATIC)
+
+
 CORS(app)  # Habilita CORS para toda la aplicación
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    api_base_url = os.getenv("API_BASE_URL", "http://localhost:3000")
+    return render_template('index.html', api_base_url=api_base_url)
 
 @app.route("/api")
 def hello_world():
